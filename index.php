be4d0278d23a9bff12a051c503c93c40d74f86c7
Nickkos <Nickkoskowski@gmail.com>
2013-11-05 08:35:38 -0800

    added initial commit!
    added index.php and basic styles

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+        <title>Koni TV</title>
+        <link rel="stylesheet" href="normalize.css" type="text/css" />
+        <link rel="stylesheet" href="style.css" type="text/css" />
+    </head>
+    <body>
+        <div class="header">
+            <div class="container">
+                <div class="logo">
+                    <img src="images/logo.jpg" alt="logo">
+                </div>
+                <div class="navigation">
+                    <ul>
+                        <li>Videos</li>
+                        <li>About Koni</li>
+                        <li>Partners</li>
+                        <li>Join KoniTV</li>
+                        <li>Events</li>
+                        <li>Blog</li>
+                        <li>Contact Us</li>
+                    </ul>
+                </div>
+                <div class="social-media">
+                </div>
+            </div>
+        </div>
+        <div class="content">
+            <div class="container">
+                <div class="slider">
+                    <div class="slide">
+                        <img src="images/sliderimage.jpg">
+                    </div>
+                </div>
+                <div class="featured-video-module">
+                    <h2>Featured Videos</h2>
+                    <div class="featured-video">
+                        <iframe width="480" height="300" src="//www.youtube.com/embed/Dd7FixvoKBw?rel=0" frameborder="0" allowfullscreen></iframe>
+                    </div>
+                    <ul class="related-videos">
+                        <li class="top"></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </div>
+                <div class="channel-module">
+                    
+                </div>
+            </div>
+        </div>
+
+    </body>
+</html>
